f6337d2f356c8d6c7cd7636c3d25f0d5ebc42624
nilver987 <nilver.1967@gmail.com>
2024-11-06 14:47:19 -0500

    first commit

---
+++ config-data/src/main/java/com/example/configdata/ConfigDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.configdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigDataApplication.class, args);
+    }
+
+}
